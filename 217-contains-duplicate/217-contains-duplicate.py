527f1c132094033d724b23eb4fbe0041799e25e8
psdev30 <68070632+psdev30@users.noreply.github.com>
2022-01-29 10:12:40 -0500

    Time: 430 ms (50.23%), Space: 24 MB (19.32%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.py
+++ 217-contains-duplicate/217-contains-duplicate.py
@@ -3,17 +3,19 @@ class Solution(object):
         #hashmap 
         #time: O(n)
         #space: O(n)
-        # tracker = dict()
-        # # iterate thru array, if new value then add to dups tracker
-        # for val in nums:
-        #     if val not in tracker:
-        #         tracker[val] = True
-        #     # if val is already in tracker, this means it was already seen and is thus a duplicate
-        #     else:
-        #         return True
-        # return False
+        tracker = dict()
+        # iterate thru array, if new value then add to dups tracker
+        for val in nums:
+            if val not in tracker:
+                tracker[val] = True
+            # if val is already in tracker, this means it was already seen and is thus a duplicate
+            else:
+                return True
+        return False
     
         #sorting
+        #time: O(nlogn + n) --> O(nlogn)
+        #space: O(1)
         nums.sort()
         for i in range(len(nums) - 1):
             if nums[i] == nums[i + 1]:

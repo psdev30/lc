4afb2242a186236a2ece621e784e0b644d944eb5
psdev30 <68070632+psdev30@users.noreply.github.com>
2022-01-29 14:19:41 -0500

    Time: 534 ms (18.74%), Space: 23 MB (52.15%) - LeetHub

--- 448-find-all-numbers-disappeared-in-an-array/448-find-all-numbers-disappeared-in-an-array.py
+++ 448-find-all-numbers-disappeared-in-an-array/448-find-all-numbers-disappeared-in-an-array.py
@@ -1,10 +1,24 @@
 class Solution(object):
     def findDisappearedNumbers(self, nums):
+        #hashmap
+        #time: O(2n) --> O(n)
+        #space: O(n)
+        # res = []
+        # n = len(nums)
+        # # use dictionary to store all numbers in array
+        # tracker = dict()
+        # for num in nums:
+        #     tracker[num] = True
+        # # iterate thru specified [1, n] range and whatever's not in the tracker must not have been in the array
+        # for i in range(1, n + 1):
+        #     if i not in tracker:
+        #         res.append(i)
+        # return res
+    
+        #set
         res = []
         n = len(nums)
-        tracker = dict()
-        for num in nums:
-            tracker[num] = True
+        tracker = set(nums)
         for i in range(1, n + 1):
             if i not in tracker:
                 res.append(i)

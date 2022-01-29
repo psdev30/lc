7b48e67849343e7f4d5e5a6bab65528603c67264
psdev30 <68070632+psdev30@users.noreply.github.com>
2022-01-29 16:16:10 -0500

    Time: 465 ms (31.85%), Space: 21.1 MB (55.61%) - LeetHub

--- 448-find-all-numbers-disappeared-in-an-array/448-find-all-numbers-disappeared-in-an-array.py
+++ 448-find-all-numbers-disappeared-in-an-array/448-find-all-numbers-disappeared-in-an-array.py
@@ -3,30 +3,43 @@ class Solution(object):
         #hashmap
         #time: O(2n) --> O(n)
         #space: O(n)
-        # res = []
-        # n = len(nums)
-        # # use dictionary to store all numbers in array
-        # tracker = dict()
-        # for num in nums:
-        #     tracker[num] = True
-        # # iterate thru specified [1, n] range and whatever's not in the tracker must not have been in the array
-        # for i in range(1, n + 1):
-        #     if i not in tracker:
-        #         res.append(i)
-        # return res
+#         res = []
+#         n = len(nums)
+#         # use dictionary to store all numbers in array
+#         tracker = dict()
+#         for num in nums:
+#             tracker[num] = True
+#         # iterate thru specified [1, n] range and whatever's not in the tracker must not have been in the array
+#         for i in range(1, n + 1):
+#             if i not in tracker:
+#                 res.append(i)
+#         return res
     
-        #set (same as hashmap just cleaner)
-        #time: O(2n) --> O(n)
-        #space: O(n)
-        # res = []
-        # n = len(nums)
-        # tracker = set(nums)
-        # for i in range(1, n + 1):
-        #     if i not in tracker:
-        #         res.append(i)
-        # return res
+#         #set (same as hashmap just cleaner)
+#         #time: O(2n) --> O(n)
+#         #space: O(n)
+#         res = []
+#         n = len(nums)
+#         tracker = set(nums)
+#         for i in range(1, n + 1):
+#             if i not in tracker:
+#                 res.append(i)
+#         return res
     
-        #set one-liner
-        return set(range(1, len(nums) + 1)) - set(nums)
+#         #set (one-liner version)
+#         return set(range(1, len(nums) + 1)) - set(nums)
+    
+        #in-place
+        res = []
+        for num in nums:
+            nums[abs(num) - 1] = abs(nums[abs(num) - 1]) * -1
+        for i, val in enumerate(nums):
+            if val > 0:
+                res.append(i + 1)
+        return res
+            
+            
+    
+        
         
         

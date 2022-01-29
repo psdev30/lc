43f8305b455c11b5e1cf508b254e66a4a252e4aa
psdev30 <68070632+psdev30@users.noreply.github.com>
2022-01-29 10:11:49 -0500

    Time: 432 ms (50.10%), Space: 22.6 MB (71.95%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.py
+++ 217-contains-duplicate/217-contains-duplicate.py
@@ -1,12 +1,21 @@
 class Solution(object):
     def containsDuplicate(self, nums):
         #hashmap 
-        tracker = dict()
-        # iterate thru array, if new value then add to dups tracker
-        for val in nums:
-            if val not in tracker:
-                tracker[val] = True
-            # if val is already in tracker, this means it was already seen and is thus a duplicate
-            else:
+        #time: O(n)
+        #space: O(n)
+        # tracker = dict()
+        # # iterate thru array, if new value then add to dups tracker
+        # for val in nums:
+        #     if val not in tracker:
+        #         tracker[val] = True
+        #     # if val is already in tracker, this means it was already seen and is thus a duplicate
+        #     else:
+        #         return True
+        # return False
+    
+        #sorting
+        nums.sort()
+        for i in range(len(nums) - 1):
+            if nums[i] == nums[i + 1]:
                 return True
         return False

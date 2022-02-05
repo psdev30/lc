637d95d19d01e4bbfbbf8e4d1ea21d2d658b24c6
psdev30 <68070632+psdev30@users.noreply.github.com>
2022-02-04 21:16:53 -0500

    Time: 129 ms (61.67%), Space: 16.4 MB (11.38%) - LeetHub

--- 136-single-number/136-single-number.py
+++ 136-single-number/136-single-number.py
@@ -3,12 +3,22 @@ class Solution(object):
         #hashmap
         #time: O(2n) --> O(n)
         #space: O(n)
-        # seen = dict()
-        # for num in nums:
-        #     seen[num] = seen.get(num, 0) + 1
-        # for num in seen:
-        #     if seen[num] == 1:
-        #         return num
+        
+        # create dict to store counts of numbers in array
+        seen = dict()
+        for num in nums:
+            # if number isn't in dict it gets initial value of 0
+            seen[num] = seen.get(num, 0) + 1
+        # whatever value in dict that's 1, its corresponding key is the 'single number'
+        for num in seen:
+            if seen[num] == 1:
+                return num
+            
         #math
+        #time: O(n)
+        #space: O(n)
         return 2 * sum(set(nums)) - sum(nums)
+    
+        #bit manipulation
+        #TODO
         

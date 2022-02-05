df364d9944c7a50d63138f9d2934acf6779cd6b4
psdev30 <68070632+psdev30@users.noreply.github.com>
2022-02-05 10:17:13 -0500

    Time: 389 ms (66.38%), Space: 24.1 MB (21.78%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.py
+++ 217-contains-duplicate/217-contains-duplicate.py
@@ -1,8 +1,16 @@
 class Solution(object):
     def containsDuplicate(self, nums):
-        nums.sort()
-        for i in range(1, len(nums)):
-            if nums[i] == nums[i - 1]:
+        # nums.sort()
+        # for i in range(1, len(nums)):
+        #     if nums[i] == nums[i - 1]:
+        #         return True
+        # return False
+    
+        tracker = dict()
+        for num in nums:
+            if num not in tracker:
+                tracker[num] = tracker.get(num, 0) + 1
+            else:
                 return True
         return False
                 

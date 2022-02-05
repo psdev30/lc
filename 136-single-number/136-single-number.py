097b38434c31cc1dd522e6dee126b3bbd44ec3a2
psdev30 <68070632+psdev30@users.noreply.github.com>
2022-02-04 20:32:46 -0500

    Time: 104 ms (92.42%), Space: 16.2 MB (29.52%) - LeetHub

--- 136-single-number/136-single-number.py
+++ 136-single-number/136-single-number.py
@@ -1,9 +1,14 @@
 class Solution(object):
     def singleNumber(self, nums):
-        seen = dict()
-        for num in nums:
-            seen[num] = seen.get(num, 0) + 1
-        for num in seen:
-            if seen[num] == 1:
-                return num
+        #hashmap
+        #time: O(2n) --> O(n)
+        #space: O(n)
+        # seen = dict()
+        # for num in nums:
+        #     seen[num] = seen.get(num, 0) + 1
+        # for num in seen:
+        #     if seen[num] == 1:
+        #         return num
+        #math
+        return 2 * sum(set(nums)) - sum(nums)
         

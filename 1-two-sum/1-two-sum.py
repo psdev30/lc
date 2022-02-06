3ec9b5093368e45b70952903aef496aa79bc350c
psdev30 <68070632+psdev30@users.noreply.github.com>
2022-02-06 12:31:49 -0500

    Time: 63 ms (70.05%), Space: 14.3 MB (72.40%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,8 +1,21 @@
 class Solution(object):
     def twoSum(self, nums, target):
         #brute force
-        for i in range(len(nums)):
-            sum = nums[i]
-            for z in range(i + 1, len(nums)):
-                if(sum + nums[z] == target):
-                    return [i, z]
+        #time: O(n^2)
+        #space: O(1)
+        # for i in range(len(nums)):
+        #     sum = nums[i]
+        #     for z in range(i + 1, len(nums)):
+        #         if(sum + nums[z] == target):
+        #             return [i, z]
+        
+        #hashmap
+        tracker = dict()
+        for i, num in enumerate(nums):
+            needed = target - num
+            if(needed in tracker):
+                return [tracker[needed], i]
+            else:
+                tracker[num] = i
+
+        

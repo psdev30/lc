5f5f2b3f3cc3ff81e6a1cc5d987c4a2319f15a5e
psdev30 <68070632+psdev30@users.noreply.github.com>
2022-02-06 12:34:34 -0500

    Time: 2895 ms (38.34%), Space: 14.5 MB (24.52%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -3,18 +3,27 @@ class Solution(object):
         #brute force
         #time: O(n^2)
         #space: O(1)
-        # for i in range(len(nums)):
-        #     sum = nums[i]
-        #     for z in range(i + 1, len(nums)):
-        #         if(sum + nums[z] == target):
-        #             return [i, z]
+        
+        # brute force method that checks thru all possible 2 num combos 
+        for i in range(len(nums)):
+            sum = nums[i]
+            for z in range(i + 1, len(nums)):
+                if(sum + nums[z] == target):
+                    return [i, z]
         
         #hashmap
+        #time: O(n)
+        #space: O(n)
+        
+        # create dictionary for O(1) search time
         tracker = dict()
+        # as we iterate thru the array, calc the needed number (target - current num)
         for i, num in enumerate(nums):
             needed = target - num
+            # then check the dict in O(1) avg. time to see if the needed value exists
             if(needed in tracker):
                 return [tracker[needed], i]
+            # if the needed value isn't there, then add the current num we're on as it might be part of the solution later
             else:
                 tracker[num] = i
 

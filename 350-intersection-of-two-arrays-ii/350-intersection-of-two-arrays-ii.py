9100a4a4a24baf93eef296c1c8214d1f87a9e33c
psdev30 <68070632+psdev30@users.noreply.github.com>
2022-02-14 20:54:27 -0500

    Time: 28 ms (97.15%), Space: 13.6 MB (62.27%) - LeetHub

--- 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.py
+++ 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.py
@@ -4,7 +4,8 @@ class Solution(object):
     def intersect(self, nums1, nums2):
         #hashmap
         #time: O(n + m)
-        #space: O(m + n)
+        #space: O(min(m, n))
+        
         if len(nums1) > len(nums2):
             return Solution.intersect(self, nums2, nums1)
         
@@ -26,21 +27,21 @@ class Solution(object):
         #time: O(nlogn + mlogm + min(n1, n2)) --> O(nlogn + mlogm)
         #space: O(n + m)
         
-#         nums1.sort()
-#         nums2.sort()
+        nums1.sort()
+        nums2.sort()
         
-#         n1 = n2 = i = 0
-#         while n1 < len(nums1) and n2 < len(nums2):
-#             if nums1[n1] == nums2[n2]:
-#                 nums1[i] = nums1[n1]
-#                 i += 1
-#                 n1 += 1
-#                 n2 += 1
-#             elif nums1[n1] > nums2[n2]:
-#                 n2 += 1
-#             else:
-#                 n1 += 1
-#         return nums1[:i]
+        n1 = n2 = i = 0
+        while n1 < len(nums1) and n2 < len(nums2):
+            if nums1[n1] == nums2[n2]:
+                nums1[i] = nums1[n1]
+                i += 1
+                n1 += 1
+                n2 += 1
+            elif nums1[n1] > nums2[n2]:
+                n2 += 1
+            else:
+                n1 += 1
+        return nums1[:i]
 
                 
         

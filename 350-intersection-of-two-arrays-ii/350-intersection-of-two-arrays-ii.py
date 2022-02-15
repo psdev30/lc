7eee1a1540603c1daf6c1104bc221e6cdcc09836
psdev30 <68070632+psdev30@users.noreply.github.com>
2022-02-14 20:31:09 -0500

    Time: 55 ms (39.35%), Space: 13.8 MB (14.60%) - LeetHub

--- 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.py
+++ 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.py
@@ -2,21 +2,43 @@ from itertools import repeat
 
 class Solution(object):
     def intersect(self, nums1, nums2):
-        nums1.sort()
-        nums2.sort()
+        #hashmap
+
+        if len(nums1) > len(nums2):
+            return Solution.intersect(self, nums2, nums1)
         
-        n1 = n2 = i = 0
-        while n1 < len(nums1) and n2 < len(nums2):
-            if nums1[n1] == nums2[n2]:
-                nums1[i] = nums1[n1]
-                i += 1
-                n1 += 1
-                n2 += 1
-            elif nums1[n1] > nums2[n2]:
-                n2 += 1
-            else:
-                n1 += 1
-        return nums1[:i]
+        counter = dict()
+        intersection = []
+        
+        for num in nums2:
+            counter[num] = counter.get(num, 0) + 1
+        for num in nums1:
+            if num in counter:
+                if counter[num] > 0:
+                    intersection.append(num)
+                    counter[num] -= 1
+        
+        return intersection
+        
+        #sorting
+        #time: O(nlogn + mlogm + min(n1, n2)) --> O(nlogn + mlogm)
+        #space: O(n + m)
+        
+#         nums1.sort()
+#         nums2.sort()
+        
+#         n1 = n2 = i = 0
+#         while n1 < len(nums1) and n2 < len(nums2):
+#             if nums1[n1] == nums2[n2]:
+#                 nums1[i] = nums1[n1]
+#                 i += 1
+#                 n1 += 1
+#                 n2 += 1
+#             elif nums1[n1] > nums2[n2]:
+#                 n2 += 1
+#             else:
+#                 n1 += 1
+#         return nums1[:i]
 
                 
         

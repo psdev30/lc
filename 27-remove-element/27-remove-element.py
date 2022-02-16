c7249722bb52153b2a6242599cc83c666654a096
psdev30 <68070632+psdev30@users.noreply.github.com>
2022-02-15 23:29:07 -0500

    Time: 43 ms (9.36%), Space: 13.4 MB (66.90%) - LeetHub

--- 27-remove-element/27-remove-element.py
+++ 27-remove-element/27-remove-element.py
@@ -1,8 +1,23 @@
 class Solution(object):
     def removeElement(self, nums, val): 
+        #slow pointer
+        #time: O(n)
+        #space: O(1)
+        
         k = 0
         for i, num in enumerate(nums):
             if num != val:
                 nums[k] = num
                 k += 1
+        return k
+    
+        #optimal
+        end = len(nums) - 1
+        k = 0
+        while k < end:
+            if nums[k] == nums[end]:
+                temp = nums[k]
+                nums[k] = nums[end]
+                nums[end] = temp
+                end -= 1
         return k

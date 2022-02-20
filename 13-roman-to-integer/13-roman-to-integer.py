e2382e220b9ea42578c07ae11f2df3e15e2b6bb7
psdev30 <68070632+psdev30@users.noreply.github.com>
2022-02-20 12:04:32 -0500

    Time: 44 ms (72.91%), Space: 13.4 MB (84.44%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.py
+++ 13-roman-to-integer/13-roman-to-integer.py
@@ -1,28 +1,40 @@
 class Solution(object):
     def romanToInt(self, s):
         roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-        total = 0
-        i = 0
+        total = i = 0
         while i < len(s):
-            if s[i] == 'I' and (i + 1 < len(s) and (s[i + 1] == 'V' or s[i + 1] == 'X')):
-                sumToAdd = roman[s[i + 1]] - roman[s[i]]
-                total += sumToAdd
+            if i + 1 < len(s) and roman[s[i]] < roman[s[i + 1]]:
+                total += roman[s[i + 1]] - roman[s[i]]
                 i += 1
-            elif s[i] == 'I':
-                total += 1
-            elif s[i] == 'X' and (i + 1 < len(s) and (s[i + 1] == 'L' or s[i + 1] == 'C')):
-                sumToAdd = roman[s[i + 1]] - roman[s[i]]
-                total += sumToAdd
-                i += 1
-            elif s[i] == 'X':
-                total += 10
-            elif s[i] == 'C' and (i + 1 < len(s) and (s[i + 1] == 'D' or s[i + 1] == 'M')):
-                sumToAdd = roman[s[i + 1]] - roman[s[i]]
-                total += sumToAdd
-                i += 1
-            elif s[i] == 'C':
-                total += 100
             else:
                 total += roman[s[i]]
             i += 1
         return total
+        
+        
+        #hashmap (the toxic af one lmao)
+        #time: O(1) (max characters limited to 15 according to constraints)
+        #space: O(1)
+        # roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        # total = 0
+        # i = 0
+        # while i < len(s):
+        #     if s[i] == 'I' and (i + 1 < len(s) and (s[i + 1] == 'V' or s[i + 1] == 'X')):
+        #         total += roman[s[i + 1]] - roman[s[i]]
+        #         i += 1
+        #     elif s[i] == 'I':
+        #         total += 1
+        #     elif s[i] == 'X' and (i + 1 < len(s) and (s[i + 1] == 'L' or s[i + 1] == 'C')):
+        #         total += roman[s[i + 1]] - roman[s[i]]
+        #         i += 1
+        #     elif s[i] == 'X':
+        #         total += 10
+        #     elif s[i] == 'C' and (i + 1 < len(s) and (s[i + 1] == 'D' or s[i + 1] == 'M')):
+        #         total += roman[s[i + 1]] - roman[s[i]]
+        #         i += 1
+        #     elif s[i] == 'C':
+        #         total += 100
+        #     else:
+        #         total += roman[s[i]]
+        #     i += 1
+        # return total

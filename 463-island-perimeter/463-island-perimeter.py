cf0e8c167bd91c18e5f0fe45161b603dd7428327
psdev30 <68070632+psdev30@users.noreply.github.com>
2022-02-19 19:25:50 -0500

    Time: 544 ms (70.16%), Space: 14 MB (33.47%) - LeetHub

--- 463-island-perimeter/463-island-perimeter.py
+++ 463-island-perimeter/463-island-perimeter.py
@@ -1,5 +1,31 @@
 class Solution(object):
     def islandPerimeter(self, grid):
+        #only checking left + up squares
+        #time: O(im)
+        #space: O(1)
+        
+        perimeter = 0
+        numRows = len(grid)
+        numCols = len(grid[0])
+        for i in range(numRows):
+            for m in range(numCols):
+                if grid[i][m] == 1:
+                    perimeter += 4
+                    if i > 0 and grid[i - 1][m] == 1:
+                        perimeter -= 2
+                    if m > 0 and grid[i][m - 1] == 1:
+                        perimeter -= 2
+        return perimeter
+        
+        
+        
+        
+        
+        
+        #check all 4 sides
+        #time: O(im)
+        #space: O(1)
+        
         numRows = len(grid)
         numCols = len(grid[0])
         perimeter = 0

b85b2cfd4315f341bf5771d8a5d3269c9c71345b
psdev30 <68070632+psdev30@users.noreply.github.com>
2022-02-20 12:14:29 -0500

    Time: 40 ms (81.67%), Space: 13.5 MB (32.49%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.py
+++ 13-roman-to-integer/13-roman-to-integer.py
@@ -1,15 +1,29 @@
 class Solution(object):
     def romanToInt(self, s):
+        #even cleaner solution
         roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-        total = i = 0
-        while i < len(s):
-            if i + 1 < len(s) and roman[s[i]] < roman[s[i + 1]]:
-                total += roman[s[i + 1]] - roman[s[i]]
-                i += 1
+        total = roman[s[len(s) - 1]]
+        for i in range(len(s) - 2, -1, -1):
+            if roman[s[i]] < roman[s[i + 1]]:
+                total -= roman[s[i]]
             else:
                 total += roman[s[i]]
-            i += 1
         return total
+        
+        
+        #better solution
+        #time: O(1) (same as below)
+        #space: O(1)
+        # roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        # total = i = 0
+        # while i < len(s):
+        #     if i + 1 < len(s) and roman[s[i]] < roman[s[i + 1]]:
+        #         total += roman[s[i + 1]] - roman[s[i]]
+        #         i += 1
+        #     else:
+        #         total += roman[s[i]]
+        #     i += 1
+        # return total
         
         
         #hashmap (the toxic af one lmao)

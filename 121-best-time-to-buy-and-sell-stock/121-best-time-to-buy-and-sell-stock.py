8db262841b2c2c75f90bf427a709be011c9d3aae
psdev30 <68070632+psdev30@users.noreply.github.com>
2022-02-21 13:40:25 -0500

    Time: 1050 ms (57.39%), Space: 22.5 MB (91.56%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
@@ -1,5 +1,8 @@
 class Solution(object):
     def maxProfit(self, prices):
+        #one pass
+        #time: O(n)
+        #space: O(1)
         maxProfit = 0
         buy = prices[0]
         for i in range(1, len(prices)):

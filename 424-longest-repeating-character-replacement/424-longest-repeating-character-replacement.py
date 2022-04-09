fa75501320dbc450aca6d1e75df81833f3a595e8
psdev30 <68070632+psdev30@users.noreply.github.com>
2022-04-09 14:52:35 -0400

    Time: 143 ms (45.40%), Space: 13.8 MB (49.69%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.py
@@ -1,11 +1,12 @@
 class Solution(object):
     def characterReplacement(self, s, k):
         tracker = dict()
-        longest = windowStart = 0
+        longest = maxRepeatingChar = windowStart = 0
         for windowEnd in range(len(s)):
             letter = s[windowEnd]
             tracker[letter] = tracker.get(letter, 0) + 1
-            while (windowEnd - windowStart + 1) - max(tracker.values()) > k:
+            maxRepeatingChar = max(maxRepeatingChar, max(tracker.values()))
+            while (windowEnd - windowStart + 1) - maxRepeatingChar > k:
                 leftLetter = s[windowStart]
                 tracker[leftLetter] -= 1
                 windowStart += 1
